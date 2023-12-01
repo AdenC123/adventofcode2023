68be905000c48d253e5bcf4d6be5f0d0e5f51413
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2023-11-30 17:06:42 -0800

    more template changes

--- template.py
+++ template.py
@@ -19,13 +19,13 @@ with open(FILENAME) as f:
 
 
 # MAIN
-def part1() -> str:
-    return ""
+def part1() -> int:
+    return 0
 
 
-def part2() -> str:
-    return ""
+def part2() -> int:
+    return 0
 
 
-print("Part 1: " + part1())
-print("Part 2: " + part1())
+print("Part 1: " + str(part1()))
+print("Part 2: " + str(part1()))
