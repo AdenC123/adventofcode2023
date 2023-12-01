d81d699a1e634c8c801a013470b9ddb397a51402
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2023-12-01 10:43:47 -0800

    wohoo more template changes

--- template.py
+++ template.py
@@ -1,28 +1,28 @@
-
-
 # CONSTANTS
-FILENAME = "test.txt"
+
 
 # STRUCTURES
 
-# GLOBALS
 
-# FILE PARSING
-with open(FILENAME) as f:
-    lines = f.readlines()
+# GLOBALS
 
 
 # HELPERS
 
 
 # MAIN
-def part1() -> int:
+def part1(lines) -> int:
     return 0
 
 
-def part2() -> int:
+def part2(lines) -> int:
     return 0
 
 
-print("Part 1: " + str(part1()))
-print("Part 2: " + str(part2()))
+def main():
+    with open("test.txt") as f:
+        print("Part 1 Test: " + str(part1(f.readlines())))
+        print("Part 2 Test: " + str(part1(f.readlines())))
+    with open("input.txt") as f:
+        print("Part 1 Input: " + str(part1(f.readlines())))
+        print("Part 2 Input: " + str(part1(f.readlines())))
