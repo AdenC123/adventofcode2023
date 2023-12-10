012f25d46b1a9ed00465b1c70fd1dfd8eeff7b39
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2023-12-09 19:48:13 -0800

    day 8 lcm idea

--- day8/day8.py
+++ day8/day8.py
@@ -31,28 +31,43 @@ def part2(lines) -> int:
     ins = lines[0].strip()
     # map node: (left, right)
     nodes = {}
+    # either a node string or an int of its cycles
     curr_nodes = []
     for line in lines[2:]:
         nodes[line[0:3]] = (line[7:10], line[12:15])
         if line[2] == 'A':
             curr_nodes.append(line[0:3])
-    # do ins
-    i = 0
+    # do ins, keep track of visited for each curr_nodes index
+    visited = {i: [node] for i, node in enumerate(curr_nodes)}
+    curr_ins = 0
     steps = 0
-    at_z = 0
-    while at_z != len(curr_nodes):
-        at_z = 0
-        for j in range(len(curr_nodes)):
-            if ins[i] == 'L':
-                curr_nodes[j] = nodes[curr_nodes[j]][0]
-            elif ins[i] == 'R':
-                curr_nodes[j] = nodes[curr_nodes[j]][1]
-            if curr_nodes[j][2] == 'Z':
-                at_z += 1
+    num_cycled = 0
+    while num_cycled != len(curr_nodes):
+        # advance all curr_nodes, watch for cycles
+        for i in range(len(curr_nodes)):
+            curr = curr_nodes[i]
+            # check if cycled already
+            if type(curr) is int:
+                continue
+            # do ins
+            if ins[curr_ins] == 'L':
+                curr_nodes[i] = nodes[curr][0]
+            elif ins[curr_ins] == 'R':
+                curr_nodes[i] = nodes[curr][1]
+            curr = curr_nodes[i]
+            if curr[2] == 'Z' and curr in visited[i]:
+                # cycle detected
+                cycle_len = len(visited[i]) - visited[i].index(curr)
+                curr_nodes[i] = cycle_len
+                num_cycled += 1
+            else:
+                visited[i].append(curr)
         steps += 1
-        if steps % 1000000 == 0:
-            print(f'\r{steps}', end='')
-        i = (i + 1) % len(ins)
+        # if steps % 1000000 == 0:
+        #     print(f'\r{steps}', end='')
+        curr_ins = (curr_ins + 1) % len(ins)
+    # TODO add LCF of the cycles? maybe need to track where in the cycle we are
+    print(curr_nodes)
     return steps
 
 
