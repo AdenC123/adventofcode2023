fd5c8c7d3bf04b9953f9d0c2fd58c12a9b794c6e
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2023-12-22 13:21:36 -0800

    day 12 speed up attempt

--- day12/day12.py
+++ day12/day12.py
@@ -10,43 +10,55 @@ import re
 
 # MAIN
 @functools.cache
-def possible(springs, nums):
-    # if all ? are not springs, is it possible to complete?
+def valid(springs, nums):
     springs = springs.replace('?', '.')
     groups = re.split(r'[.]+', springs)
-    groups = [g for g in groups if len(g) != 0]
+    nums_idx = 0
     for group in groups:
-        if len(group) not in nums:
-            return False
+        if len(group) != 0:
+            if nums_idx == len(nums) or len(group) != nums[nums_idx]:
+                return False
+            nums_idx += 1
     return True
 
 
 @functools.cache
-def valid(springs, nums):
-    # assume no ? in springs
-    groups = re.split(r'[.]+', springs)
-    groups = [g for g in groups if len(g) != 0]
-    if len(nums) != len(groups):
-        return False
-    for n, group in zip(nums, groups):
-        if len(group) != n:
-            return False
-    return True
+def num_combinations(springs: str, nums: tuple, nums_idx: int, springs_idx: int) -> int:
+    # iterate through line, placing first group in all possible positions, then recursing
+    # keep track of past index allowed to place the next group
 
+    # starting from idx, iterate forward to find all places to put next group, replacing ? with .
+    # each time position is found, recurse with that ? replaced and the rest of nums
 
-@functools.cache
-def num_combinations(springs: str, nums: tuple) -> int:
-    # base case: all springs placed and valid
-    # if not possible(springs, nums):
-    #     return 0
-    if '?' not in springs:
+    # base case: no nums left
+    if nums_idx == len(nums):
         if valid(springs, nums):
+            # print(springs.replace('?', '.'))
             return 1
         return 0
-    # place a spring and a dot in the first slot and recurse
-    num_dot = num_combinations(springs.replace('?', '.', 1), nums)
-    num_spring = num_combinations(springs.replace('?', '#', 1), nums)
-    return num_dot + num_spring
+    # try to place in all spots
+    n = nums[nums_idx]
+    rsf = 0
+    for start in range(springs_idx, len(springs) - n + 1):
+        end = start + n
+        area = springs[start:end]
+        if area.replace('?', '#') != '#' * n:
+            # invalid area
+            continue
+        if end >= len(springs):
+            # next is off edge
+            if nums_idx == len(nums) - 1 and (start - 1 < 0 or springs[start-1] in '?.'):
+                # used last num, finished combo (if valid)
+                new_springs = springs[:start].replace('?', '.') + '#' * n
+                if valid(new_springs, nums):
+                    # print(new_springs)
+                    rsf += 1
+            continue
+        if springs[end] in '?.' and (start - 1 < 0 or springs[start-1] in '?.'):
+            # valid area, place group here
+            new_springs = springs[:start] + '#' * n + '.' + springs[end+1:]
+            rsf += num_combinations(new_springs, nums, nums_idx + 1, end+1)
+    return rsf
 
 
 def part1(lines) -> int:
@@ -54,8 +66,9 @@ def part1(lines) -> int:
     for line in lines:
         springs, nums = line.strip().split()
         nums = tuple(map(int, nums.split(',')))
-        combos = num_combinations(springs, nums)
+        combos = num_combinations(springs, nums, 0, 0)
         print(combos)
+        # print()
         rsf += combos
     return rsf
 
@@ -79,12 +92,12 @@ def part2(lines) -> int:
 def main():
     with open("test.txt") as f:
         lines = f.readlines()
-        # print("Part 1 Test: " + str(part1(lines)))
-        print("Part 2 Test: " + str(part2(lines)))
+        print("Part 1 Test: " + str(part1(lines)))
+        # print("Part 2 Test: " + str(part2(lines)))
     with open("input.txt") as f:
         lines = f.readlines()
         # print("Part 1 Input: " + str(part1(lines)))
-        # print("Part 2 Input: " + str(part2(lines)))
+        print("Part 2 Input: " + str(part2(lines)))
 
 
 if __name__ == '__main__':
